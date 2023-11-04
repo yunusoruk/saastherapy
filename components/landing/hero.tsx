03d8ca01494aad80b8ec05a653b585e497d4de9e
yunusoruk <orukyunus@gmail.com>
2023-11-04 10:02:34 +0300

    fix: hero

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -20,11 +20,8 @@ export const Hero: React.FC = () => {
                 href="/companion"
                 className="inline-flex items-center rounded-lg bg-muted px-3 py-1 text-sm font-medium cursor-pointer"
               >
-                <span className="sm:hidden">Explore the World of SaaS Therapy</span>
-                <span className="hidden sm:inline ">
-                  Explore the World of SaaS Therapy{" "}
-                </span>
-                <Icons.arrowRight className="ml-1 h-4 w-4" />
+                <span>Explore the World of SaaS Therapy</span>
+                <Icons.arrowRight className="ml-1 h-4 w-4 hidden sm:inline" />
               </Link>
             </div>
             <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl pb-4" data-aos="fade-down">
@@ -36,11 +33,11 @@ export const Hero: React.FC = () => {
             <p className="mb-8 text-lg md:text-xl text-muted-foreground " data-aos="fade-down" data-aos-delay="200">
               Empower Psychology Students with AI-Powered Practice Sessions
             </p>
-            <div className="space-x-4">
-              <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+            <div className="flex flex-col items-center justify-center sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
+              <Link href="/login" className={cn(buttonVariants({ size: 'lg', variant: 'outline', className: "w-[180px] bg-primary" }))}>
                 Get Started
               </Link>
-              <Link href={siteConfig.links.twitter} target="_blank" className={cn(buttonVariants({ size: "lg", variant: 'outline' }))}>
+              <Link href={siteConfig.links.twitter} target="_blank" className={cn(buttonVariants({ size: "lg", variant: 'outline', className: "w-[180px]" }))}>
                 Fallow along on X
               </Link>
             </div>

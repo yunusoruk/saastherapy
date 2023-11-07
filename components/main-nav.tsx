643641623e8773f3a3536e31a75c7932b622b7f2
yunusoruk <orukyunus@gmail.com>
2023-11-07 12:57:54 +0300

    update:logo

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -24,8 +24,12 @@ export function MainNav({ items, children }: MainNavProps) {
     return (
         <div className="flex gap-6 md:gap-10">
             <Link href="/" className="hidden items-center space-x-2 md:flex">
-                <Icons.logo className="text-primary" />
-
+                <svg width="37.5" height="20" viewBox="0 0 360 192" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M360 144C360 170.51 338.51 192 312 192C285.49 192 264 170.51 264 144C264 117.49 285.49 96 312 96C338.51 96 360 117.49 360 144Z" fill="#c2def2" />
+                    <path d="M312 96C312 149.019 269.019 192 216 192C162.981 192 120 149.019 120 96C120 42.9807 162.981 4.67745e-06 216 4.67745e-06C269.019 4.67745e-06 312 42.9807 312 96Z" fill="#228fda" />
+                    <path d="M192 120C192 159.765 159.765 192 120 192C80.2355 192 48 159.765 48 120C48 80.2355 80.2355 48 120 48C159.765 48 192 80.2355 192 120Z" fill="#5fa8db" />
+                    <path d="M96 144C96 170.51 74.5097 192 48 192C21.4903 192 0 170.51 0 144C0 117.49 21.4903 96 48 96C74.5097 96 96 117.49 96 144Z" fill="#95c6e8" />
+                </svg>
                 <p className="hidden font-bold sm:inline-block text-xl  ">
                     <span className="text-primary">
                         SaaS
@@ -56,7 +60,17 @@ export function MainNav({ items, children }: MainNavProps) {
                 className="flex items-center space-x-2 md:hidden"
                 onClick={() => setShowMobileMenu(!showMobileMenu)}
             >
-                {showMobileMenu ? <X /> : <Icons.logo className="text-primary" />}
+                {showMobileMenu
+                    ?
+                    <X />
+                    :
+                    <svg width="37.5" height="20" viewBox="0 0 360 192" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M360 144C360 170.51 338.51 192 312 192C285.49 192 264 170.51 264 144C264 117.49 285.49 96 312 96C338.51 96 360 117.49 360 144Z" fill="#c2def2" />
+                        <path d="M312 96C312 149.019 269.019 192 216 192C162.981 192 120 149.019 120 96C120 42.9807 162.981 4.67745e-06 216 4.67745e-06C269.019 4.67745e-06 312 42.9807 312 96Z" fill="#228fda" />
+                        <path d="M192 120C192 159.765 159.765 192 120 192C80.2355 192 48 159.765 48 120C48 80.2355 80.2355 48 120 48C159.765 48 192 80.2355 192 120Z" fill="#5fa8db" />
+                        <path d="M96 144C96 170.51 74.5097 192 48 192C21.4903 192 0 170.51 0 144C0 117.49 21.4903 96 48 96C74.5097 96 96 117.49 96 144Z" fill="#95c6e8" />
+                    </svg>
+                }
                 <span className="font-bold">Menu</span>
             </button>
             {showMobileMenu && items && (
--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,17 +1,18 @@
 import * as React from "react"
-
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
-import { Command as Logo } from "lucide-react"
-import { ModeToggle } from "@/components/mode-toggle"
-import { Icons } from "./icons"
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
     return (
         <footer className={cn(className)}>
             <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-                    <Icons.logo />
+                    <svg width="37.5" height="20" viewBox="0 0 360 192" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M360 144C360 170.51 338.51 192 312 192C285.49 192 264 170.51 264 144C264 117.49 285.49 96 312 96C338.51 96 360 117.49 360 144Z" fill="#c2def2" />
+                        <path d="M312 96C312 149.019 269.019 192 216 192C162.981 192 120 149.019 120 96C120 42.9807 162.981 4.67745e-06 216 4.67745e-06C269.019 4.67745e-06 312 42.9807 312 96Z" fill="#228fda" />
+                        <path d="M192 120C192 159.765 159.765 192 120 192C80.2355 192 48 159.765 48 120C48 80.2355 80.2355 48 120 48C159.765 48 192 80.2355 192 120Z" fill="#5fa8db" />
+                        <path d="M96 144C96 170.51 74.5097 192 48 192C21.4903 192 0 170.51 0 144C0 117.49 21.4903 96 48 96C74.5097 96 96 117.49 96 144Z" fill="#95c6e8" />
+                    </svg>
                     <p className="text-center text-sm leading-loose md:text-left">
                         Follow us{" "}
                         <a
@@ -26,7 +27,6 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
                     </p>
                 </div>
                 <div className=""></div>
-                {/* <ModeToggle /> */}
             </div>
         </footer>
     )

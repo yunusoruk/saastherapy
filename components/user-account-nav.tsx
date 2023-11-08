5fb77417fee841a05ff3430a11a7e420dc30fdf4
yunusoruk <orukyunus@gmail.com>
2023-11-08 17:32:30 +0300

    fix:dashboard

--- components/user-account-nav.tsx
+++ components/user-account-nav.tsx
@@ -39,10 +39,7 @@ export function UserAccountNav({ user }: UserAccountNavProps) {
                 </div>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem asChild>
-                    <Link href="/dashboard">Dashboard</Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                    <Link href="/dashboard/billing">Billing</Link>
+                    <Link href="/dashboard/billing" >Billing</Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
                     <Link href="/dashboard/settings">Settings</Link>

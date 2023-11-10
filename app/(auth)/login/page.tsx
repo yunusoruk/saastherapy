c47c2f18219ba0e4817fc11fb5e8880d2e3c9f33
yunusoruk <orukyunus@gmail.com>
2023-11-10 15:37:17 +0300

    fix:auth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -38,7 +38,7 @@ export default function LoginPage() {
                         Welcome back
                     </h1>
                     <p className="text-sm text-muted-foreground">
-                        Enter your email to sign in to your account
+                        Continue with your account
                     </p>
                 </div>
                 <UserAuthForm />
